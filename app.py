e9ef29c8a39917253aab4890a8d8dfbe42da5647
pniedzialomski <niedzialomski1@gmail.com>
2023-04-01 21:07:04 +0200

    dodalem fajna wiadomosc

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello, world!"
+    return "hello, world! hahaha ale jaja jak berety"
 
 if __name__ == "__main__":
     app.run(debug=True)
